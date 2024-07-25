275b40a5f6f21489e323684edbe0846943327757
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2024-07-25 08:13:01 +0700

    update login page

--- src/pages/auth/Signin2.jsx
+++ src/pages/auth/Signin2.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Button, Card, Col, Form, Image, Row } from "react-bootstrap";
-import { useLogin } from "../../hooks/useAuth";
-import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import * as Yup from "yup";
-import SEO from "../../components/SEO";
-import InputWrapper from "../../components/InputWrapper";
+import React, { useEffect } from "react";
+import { Button, Card, Col, Form, Image, Row } from "react-bootstrap";
 import ReCAPTCHA from "react-google-recaptcha";
-import "../../assets/css/login.css"; // Import CSS here
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import BarantinaLogo from "../../assets/img/1.png"; // Correct path
+import BackgroundImage from "../../assets/img/12.jpg"; // Correct path
+import KemenkeuLogo from "../../assets/img/3.png"; // Correct path
+import InputWrapper from "../../components/InputWrapper";
+import SEO from "../../components/SEO";
+import { useLogin } from "../../hooks/useAuth";
 
 const RECAPTCHA_SITE_KEY = "6LdceBUqAAAAAKVabEGH7mAhufEqB0_KDIdvSwfL";
 
@@ -44,13 +46,10 @@ const Signin2 = () => {
 			if (response.success) {
 				navigate("/");
 			} else {
-				// toast(
-				//  "Login failed. Please check your username and password.",
-				//  "Error"
-				// );
+				// Handle login failure
 			}
 		} catch (error) {
-			// toast(error.message || "An error occurred", "Error");
+			// Handle error
 		}
 	};
 
@@ -69,228 +68,60 @@ const Signin2 = () => {
 		<div className="background-custom">
 			<SEO title="Login" />
 			<Row className="g-0" style={{ height: "100vh", width: "100%" }}>
-				<Col md="6" className="left-col">
-					<div className="p-4">
-						<Col md="12">
-							<div className="logo-header">
-								<Image src="/keuangan.png" width={350} height={90} />
+				<Col md={4} className="d-flex align-items-center justify-content-center">
+					<Card className="w-95" style={{ backgroundColor: 'white' , width:'400px', marginTop:'20px'}}> {/* Background color for the entire card */}
+						<Card.Header className="text-center" style={{ backgroundColor: 'white', color: 'white', borderBottom: 'none' , height:'200px'}}>
+							{/* No border between header and body */}
+							<div className="d-flex mb-3" style={{ marginLeft: "60px", marginBottom: "8px" }}>
+								<Image src={BarantinaLogo} width={100} height={100} className="me-3" />
+								<Image src={KemenkeuLogo} width={150} height={100} />
 							</div>
-						</Col>
-						<Col md="12" style={{ marginLeft: "30px" }}>
-							<div className="logo-header">
-								<div className="mt-4">
-									<h2 style={{ color: "#FFA500" }}>Persyaratan</h2>
-									<h1 className="fw-bold" style={{ color: "#c9870e" }}>
-										Pengunaan Layanan
-									</h1>
-								</div>
-							</div>
-						</Col>
+							<h1 className="fw-bold mt-3" style={{ color: "#c9870e", marginTop: "8px" }}>E-PNBP</h1>
+							<p style={{ color: "#c9870e", marginTop: "8px" }}>Cepat, Mudah, Terintegrasi</p>
 
-						<div
-							className="color-circles mt-4"
-							style={{ display: "flex", gap: "24px", marginLeft: "30px" }}
-						>
-							<div
-								className="color-circle-container"
-								style={{
-									display: "flex",
-									flexDirection: "column",
-									alignItems: "center",
-								}}
-							>
-								<div
-									className="color-circle"
-									style={{
-										backgroundColor: "#FFA500",
-										width: "180px",
-										height: "180px",
-										borderRadius: "50%",
-									}}
-								></div>
-								<span
-									style={{
-										marginTop: "30px",
-										textAlign: "center",
-										display: "block",
-									}}
-								>
-									Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
-									eiusmod tempor incididunt ut labore et
-								</span>
-							</div>
-							<div
-								className="color-circle-container"
-								style={{
-									display: "flex",
-									flexDirection: "column",
-									alignItems: "center",
-								}}
-							>
-								<div
-									className="color-circle"
-									style={{
-										backgroundColor: "#FF6347",
-										width: "180px",
-										height: "180px",
-										borderRadius: "50%",
-									}}
-								></div>
-								<span
-									style={{
-										marginTop: "30px",
-										textAlign: "center",
-										display: "block",
-									}}
-								>
-									Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
-									eiusmod tempor incididunt ut labore et
-								</span>
-							</div>
-							<div
-								className="color-circle-container"
-								style={{
-									display: "flex",
-									flexDirection: "column",
-									alignItems: "center",
-								}}
-							>
-								<div
-									className="color-circle"
-									style={{
-										backgroundColor: "#40E0D0",
-										width: "180px",
-										height: "180px",
-										borderRadius: "50%",
-									}}
-								></div>
-								<span
-									style={{
-										marginTop: "30px",
-										textAlign: "center",
-										display: "block",
-									}}
-								>
-									Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
-									eiusmod tempor incididunt ut labore et
-								</span>
-							</div>
-							<div
-								className="color-circle-container"
-								style={{
-									display: "flex",
-									flexDirection: "column",
-									alignItems: "center",
-								}}
-							>
-								<div
-									className="color-circle"
-									style={{
-										backgroundColor: "#4169E1",
-										width: "180px",
-										height: "180px",
-										borderRadius: "50%",
-									}}
-								></div>
-								<span
-									style={{
-										marginTop: "30px",
-										textAlign: "center",
-										display: "block",
-									}}
-								>
-									Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
-									eiusmod tempor incididunt ut labore et
-								</span>
-							</div>
-						</div>
-
-						<div style={{ marginLeft: "30px" }}>
-							<p className="mt-3">Data yang diperlukan</p>
-							<ol>
-								<li>Lorem ipsum dolor sit amet</li>
-								<li>Lorem ipsum dolor sit amet</li>
-								<li>Lorem ipsum dolor sit amet</li>
-								<li>Lorem ipsum dolor sit amet</li>
-							</ol>
-						</div>
-					</div>
-				</Col>
-
-				<Col
-					md="6"
-					className="right-col d-flex align-items-center justify-content-center"
-				>
-					<Card className="card-sign w-75">
-						<Card.Header
-							className="text-center"
-							style={{ marginLeft: "100px" }}
-						>
-							<Col md="12">
-								<div className="logo-header">
-									<Image src="/logo.png" width={150} height={150} />
-									<div className="ml-3">
-										<h1 className="fw-bold mt-5" style={{ color: "#c9870e" }}>
-											BARANTIN
-										</h1>
-										<p className="text-center" style={{ color: "#FFA500" }}>
-											Cepat, Mudah, Terintegrasi
-										</p>
-									</div>
-								</div>
-							</Col>
 						</Card.Header>
-						<Card.Body>
+						<Card.Body style={{ backgroundColor: 'white' }}> {/* Same background color as header */}
 							<Form onSubmit={handleSubmit(onSubmit)}>
-								<div className="mb-4">
-									<InputWrapper
-										error={!!errors?.username}
-										message={errors?.username?.message}
-									>
+								<div className="mb-3">
+									<InputWrapper error={!!errors?.username} message={errors?.username?.message}>
 										<Form.Label>Username</Form.Label>
 										<Form.Control
 											{...register("username")}
 											type="text"
 											placeholder="Enter your username"
 											autoComplete="off"
+											style={{ height: "calc(2.5em + 2px)" }} // Adjust height as needed
 										/>
 									</InputWrapper>
 								</div>
-								<div className="mb-4">
-									<InputWrapper
-										error={!!errors?.password}
-										message={errors?.password?.message}
-									>
-										<Form.Label className="d-flex justify-content-between">
-											Password <Link to="">Forgot password?</Link>
-										</Form.Label>
+								<div className="mb-3">
+									<InputWrapper error={!!errors?.password} message={errors?.password?.message}>
+										<Form.Label>Password</Form.Label>
 										<Form.Control
 											{...register("password")}
 											type="password"
 											placeholder="Enter your password"
+											style={{ height: "calc(2.5em + 2px)" }} // Adjust height as needed
 										/>
 									</InputWrapper>
 								</div>
-								<div className="mb-4">
-									<InputWrapper
-										error={!!errors?.recaptcha}
-										message={errors?.recaptcha?.message}
-									>
-										<div className="recaptcha-container" style={{ width: "100%" }}>
+								<div className="mb-3">
+									<InputWrapper error={!!errors?.recaptcha} message={errors?.recaptcha?.message}>
+										<div className="recaptcha-container" style={{ width: "100%", height: "78px" }}> {/* Set height to match CAPTCHA */}
 											<ReCAPTCHA
 												sitekey={RECAPTCHA_SITE_KEY}
 												onChange={handleRecaptcha}
+												size="normal"
 											/>
 										</div>
 									</InputWrapper>
 								</div>
-								<Button type="submit" className="btn-sign w-100">
-									Sign In
-								</Button>
+								<Button type="submit" className="w-100" style={{ height: "calc(2.5em + 2px)" }}>Sign In</Button> {/* Adjust button height */}
 							</Form>
 						</Card.Body>
 					</Card>
 				</Col>
+				<Col md={8} style={{ backgroundImage: `url(${BackgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh' }} />
 			</Row>
 		</div>
 	);
